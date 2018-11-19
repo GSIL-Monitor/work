fd8be0a99e373d6922aded8eacc1a6861f7de1ff
monkey-devops <39786197+monkey-devops@users.noreply.github.com>
2018-11-19 14:08:54 +0800

    Add files via upload

---
+++ seer/db/manage_database.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in 334 405 479 517 531 533 549 550 551 552 557 558 559 572 577 582 584 587 590 594 599 604 612 621 626 631
+do
+#/usr/local/mysql/bin/mysqladmin -uroot -ppassword -S /tmp/mysqlseer_game$i.sock shutdown
+#/usr/local/mysql/bin/mysqld_multi start 3$i
+#/usr/local/mysql/bin/mysql -uroot -ppassword -S /tmp/mysqlseer_game$i.sock -e "show slave status\G"|grep "Seconds_Behind_Master" 
+done
