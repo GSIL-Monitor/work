dd509cb4039613c2e68f3ac25abaf778f8cff0fc
fengzhiqiang <monkey_devops@163.com>
2018-12-29 17:52:15 +0800

    update

---
+++ seer/auto_open_gamserver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export HOME=/root
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/usr/local/erlang/bin
+cd /root/server/gameserver632
+/bin/bash start.sh
+exit 0
